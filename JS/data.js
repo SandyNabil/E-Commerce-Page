a3f4a082cb65ffba777f918bac41c6aba2d4a5a3
SandyNabil <sandynabil257@gmail.com>
2026-02-12 02:08:51 +0200

    HTML Structure
    added the content of the page (html only)

---
+++ JS/data.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        id: 1,
+        name: "Elegant Diamond Solitaire",
+        price: "$1,500",
+        imgSrc: "./assets/images/product1.png"
+    },
+    {
+        id: 2,
+        name: "Vintage Rose Gold Band",
+        price: "$2,300",
+        imgSrc: "./assets/images/product2.png"
+    },
+    {
+        id: 3,
+        name: "=Gold Wedding Ring",
+        price: "$980",
+        imgSrc: "./assets/images/product3.png"
+    },
+    {
+        id: 4,
+        name: "Modern Engagement Ring",
+        price: "$1,200",
+        imgSrc: "./assets/images/product4.png"
+    }
+];
+export default products ;
